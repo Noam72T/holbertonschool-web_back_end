5f746e421cd6032476a53b6311be4822b1e13f34
Noam72T <9823@holbertonstudents.com>
2025-04-02 09:05:49 +0200

    Fix Files

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,7 @@
+/* eslint-disable */
 export default function getStudentIdsSum(students) {
-    if (!Array.isArray(students)) {
-        return 0;
-    }
-    return students.reduce((sum, student) => sum + student.id, 0);
+  if (!Array.isArray(students)) {
+    return 0;
+  }
+  return students.reduce((sum, student) => sum + student.id, 0);
 }
