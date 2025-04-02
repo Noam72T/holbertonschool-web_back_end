a0293f7e5a66045548dc49a478a2f9c8dac005ed
Noam72T <9823@holbertonstudents.com>
2025-04-02 20:31:28 +0200

    fix files

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,17 +1,31 @@
-/* eslint-disable */
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-      .allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName),
-      ])
-      .then((results) => results.map((result) => ({
-                                       status: result.status,
-                                       value: result.status === 'fulfilled' ?
-                                           result.value :
-                                           result.reason,
-                                     })));
+export default async function handleProfileSignup(
+    firstName, lastName, fileName) {
+  const user = {
+    status: 'pending',
+    value: '',
+  };
+  const photo = {
+    status: 'pending',
+    value: '',
+  };
+  try {
+    const signup = await signUpUser(firstName, lastName);
+    user.status = 'fulfilled';
+    user.value = signup;
+  } catch (error) {
+    user.status = 'rejected';
+    user.value = error.toString();
+  }
+  try {
+    const upload = await uploadPhoto(fileName);
+    photo.status = 'fulfilled';
+    photo.value = upload;
+  } catch (error) {
+    photo.status = 'rejected';
+    photo.value = error.toString();
+  }
+  return [user, photo];
 }
