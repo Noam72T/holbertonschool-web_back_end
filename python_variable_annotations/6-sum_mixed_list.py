d80b439d73b83bb720f593666570df2c02260bd6
Noam72T <9823@holbertonstudents.com>
2025-04-23 10:08:42 +0200

    Fix all Files

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
-"""Function - Mixed list sum."""
+"""Function - mixed list"""
 
-def sum_mixed_list(mxd_lst: list[float]) -> float:
-    """Returns the sum of a mixed list."""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    return a sum of all nums inside a list
+    """
     return sum(mxd_lst)
