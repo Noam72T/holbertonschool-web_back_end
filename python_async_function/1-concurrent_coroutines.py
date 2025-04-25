f39b8980dcc3b6f8f0672a956d819d610d2d214d
Noam72T <9823@holbertonstudents.com>
2025-04-25 12:00:28 +0200

    Fix all

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python3
 import asyncio
 import random
 from typing import List
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random delay between 0 and max_delay and return it."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times and return list of delays in ascending order."""
--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
 import time
 import asyncio
 from typing import Callable
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
+
 
 def measure_time(n: int, max_delay: int) -> float:
     """Measure the average time taken to run wait_n(n, max_delay)."""
