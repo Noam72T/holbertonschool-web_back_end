6a66ac83012c1bb88cd05a0bb55584a98b603d86
Noam72T <9823@holbertonstudents.com>
2025-04-25 12:04:14 +0200

    fix all tasks

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,14 +5,16 @@ from typing import List
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Wait for a random delay between 0 and max_delay and return it."""
+    """Wait for a random delay between 0 """
+    """and max_delay and return it."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn wait_random n times and return list of delays in ascending order."""
+    """Spawn wait_random n times and return"""
+    """list of delays in ascending order."""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delays = []
 
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,7 +6,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn task_wait_random n times and return list of delays in ascending order."""
+    """Spawn task_wait_random n times"""
+    """and return list of delays in ascending order."""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
 
