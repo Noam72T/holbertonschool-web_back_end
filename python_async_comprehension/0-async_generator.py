0d9a114542c63b3544f1379c315c514da33805f6
Noam72T <9823@holbertonstudents.com>
2025-04-28 09:33:13 +0200

    Task 0  Work

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -17,6 +17,3 @@ async def async_generator():
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.randint(0, 10)
