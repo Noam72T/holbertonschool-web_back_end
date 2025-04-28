5f59c52ef1ec4e1ae6fd585bd23b6fad9f5d6e73
Noam72T <9823@holbertonstudents.com>
2025-04-28 09:36:29 +0200

    Task 0  Work

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Asynchronously generates 10 random numbers between 0 and 10.
 
