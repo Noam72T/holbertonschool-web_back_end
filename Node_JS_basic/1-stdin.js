f2dad173c39e438b10b7a0d4431b8904b91b070b
Noam72T <9823@holbertonstudents.com>
2025-05-19 10:49:13 +0200

    Try task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
-    const name = data.trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    
-    });
-
-process.on('exit', () => {
-  console.log('This important software is now closing');
+  process.stdout.write(`Your name is: ${data}`);
 });
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
+
+
